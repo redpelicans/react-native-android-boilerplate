2e14a872c6a8e888c56369f25278d2ff8a2aa075
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-03-07 10:52:42 +0100

    gulp chain for css is ready soon.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
+var gulp = require('gulp')
+var nodemon = require('gulp-nodemon')
+var watch = require('gulp-watch')
+var css = require('gulp-react-native-stylesheet-css')
 
+gulp.task('default', ['dev'])
 
-/*
-gulp.task('dev', ["lol"], function(){
+gulp.task('dev', ["watch-css"], function(){
   nodemon({
     script: 'node_modules/react-native/local-cli/cli.js',
     args: ["start"],
-    ignore: ["."],
-    tasks: function(file) {
-      console.log("---------- TASK : ", file)
-      return []
-    }
+    ignore: ["*"]
   })
 })
 
-
-gulp.task('lol', function(){
-  console.log("--------- COOL --------")
+gulp.task('watch-css', function(cb){
+  cb()
+  console.log("--------- START TASK --------")
+  watch("./app/**/*.css", function(){
+    gulp.src("./app/**/*.css")
+      .pipe(css({ withExtendedStyleSheet: true }))
+      .pipe(gulp.dest('build'))
+    console.log("--------- TRANSFORM CSS TO JS --------")
+  })
+  console.log("--------- END TASK --------")
 })
-*/
